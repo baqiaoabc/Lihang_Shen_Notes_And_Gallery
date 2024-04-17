d04193e902473a771b42de0e6446f53a6e9324dd
lfy <1053200896@qq.com>
2024-04-17 16:48:26 +0800

    Update CSC207 Project

---
+++ Projects/CSC207-Software-Design-main/phase2/src/gateway/Config.java
@@ -0,0 +1,26 @@
+package gateway;
+
+/**
+ * @program: group_0173
+ * @description: Configuration of DATABASE.
+ * @create: 2020-11-30 14:33
+ **/
+public class Config {
+<<<<<<< HEAD
+    static final String DATABASE_URL = "url";
+    static final int DATABASE_PORT = 00;
+    static final String DATABASE_PASSWORD = "password";
+=======
+    static final String DATABASE_URL = "207.246.94.177";
+    static final int DATABASE_PORT = 6379;
+    static final String DATABASE_PASSWORD = "207207";
+>>>>>>> master
+
+    static final String NEXT_USER_ID = "next_user_id";
+    static final String NEXT_EVENT_ID = "next_event_id";
+    static final String NEXT_ROOM_ID = "next_room_id";
+    static final String USER_HASH = "user_hash";
+    static final String EVENT_HASH = "event_hash";
+    static final String ROOM_HASH = "room_hash";
+    static final String MESSAGE_LIST = "message_list";
+}
